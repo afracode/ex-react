6e3b97ef56183a779806f7919ea341b806bcccd6
Ali-Sharifi-Neyestani <hacka.kootlas@gmail.com>
2020-03-31 13:51:30 +0430

    minor modify

--- src/examples/hooks/Count.js
+++ src/examples/hooks/Count.js
@@ -1,8 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Count() {
 
     const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        document.title = `You clicked ${count} times`;
+    });
 
     return (
         <div>
